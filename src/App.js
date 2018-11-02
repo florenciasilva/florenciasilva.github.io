3380f29de9642df817c8994c46207c0a379c5621
florenciasilva <hensgrej@gmail.com>
2018-11-01 22:53:30 -0600

    Card Lateral

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Presentation from './Components/Presentation'
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <Presentation />
+    );
+  }
+}
+
+export default App;
---
+++ src/Components/Presentation.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Card, Col, Row, Icon, Button} from 'react-materialize';
+
+
+const Presentation = () => (
+    <div>
+        <Row>
+        <Col s={10} m={3} l={3}>
+        <Card>
+            <h2 className='center'>Florencia Silva Olivera</h2>
+            <h5>Soy una Front End Developer con
+                conocimientos de JavaScript, ReactJS y React-Native
+                con 6 meses de experiencia</h5>
+            <p>Laboratorian <span className="bold">|</span> Agile <span className="bold">|</span> Autodidacta</p>
+        </Card>
+
+        </Col>
+        </Row>
+    </div>
+);
+
+export default Presentation
