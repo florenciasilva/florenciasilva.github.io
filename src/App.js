0afb87250f18fe7e5618b4c72bb717ecc5c6b159
florenciasilva <hensgrej@gmail.com>
2018-11-02 02:04:42 -0600

    Sección Proyectos con scroll sin terminar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import Presentation from './Components/Presentation'
+import Presentation from './Components/Presentation';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
      <Presentation />
+     </div> 
     );
   }
 }
--- src/Components/Presentation.js
+++ src/Components/Presentation.js
@@ -1,20 +1,37 @@
 import React from 'react';
-import {Card, Col, Row, Icon, Button} from 'react-materialize';
+import {Card, Col, Row, Button} from 'react-materialize';
+import Projects from './Projects';
 
 
 const Presentation = () => (
     <div>
         <Row>
         <Col s={10} m={3} l={3}>
-        <Card>
+        <Card className="yellow-card">
             <h2 className='center'>Florencia Silva Olivera</h2>
             <h5>Soy una Front End Developer con
                 conocimientos de JavaScript, ReactJS y React-Native
                 con 6 meses de experiencia</h5>
             <p>Laboratorian <span className="bold">|</span> Agile <span className="bold">|</span> Autodidacta</p>
+            <Row>
+            <Button><a href="https://drive.google.com/file/d/16TrK313KGCVTyV___0vPYC6bYMOmpupo/view?usp=sharing" target="_blank" rel="noopener noreferrer">Descarga mi CV</a></Button>
+            </Row>
+            <div className="margin-links">
+            <Row>
+                <a href="https://github.com/florenciasilva" target="_blank" rel="noopener noreferrer" className="black-text links"><i class="fab fa-github-square"></i> github/florenciasilva</a>
+            </Row>
+            <Row>
+                <a href="https://linkedin.com/in/florenciasilvaolivera" target="_blank" rel="noopener noreferrer" className="black-text links"><i class="fab fa-linkedin"></i> linkedin/florenciasilvaolivera</a>
+            </Row>
+            <Row>
+                <a href="mailto:hensgrej@gmail.com" target="_blank" rel="noopener noreferrer" className="black-text links"><i class="fas fa-envelope-square"></i> hensgrej@gmail.com</a>
+            </Row>
+            </div>
         </Card>
 
         </Col>
+        <Projects />
+
         </Row>
     </div>
 );
