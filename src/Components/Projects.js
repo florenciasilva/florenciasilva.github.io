55ca2cca3da1a4b9041197a2df363119f83bfd80
florenciasilva <hensgrej@gmail.com>
2019-03-31 20:01:43 -0600

    Se cambian proyectos y se agregan imagenes

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,52 +1,60 @@
 import React from 'react';
 import {Card, Col, Row, CardTitle} from 'react-materialize';
+import Reactibook from '../assets/reactibook-portfolio.JPG'
+import Cipher from '../assets/cipher-portfolio.JPG';
+import Seventh from '../assets/seventh-portfolio.JPG';
+import Pokedeck from '../assets/pokedeck-portfolio.JPG';
 
 const Projects = () => (
         <Col s={10} m={9} l={9}>
             <Card className="scroll">
                 <h3 className="center">Proyectos</h3>
                 <Row>
+                    <Col m={4} l={4}>
+                    <Card className='small'
+                        header={<CardTitle image= {Reactibook}></CardTitle>}>
+                            <div className="center">
+                                <a href="https://github.com/florenciasilva/reactibook" className="title">Reactibook</a>
+                                <div>Red Social creada con React, Redux y Firebase.</div>
+                            </div>
+                    </Card>
+                    </Col>
+
                 <Col m={4} l={4}>
                     <Card className='small'
                         header={<CardTitle image='https://i.imgur.com/IC26J8Z.png'></CardTitle>}>
                             <div className="center">
-                            <a href="https://github.com/florenciasilva/cdmx-2018-06-bc-core-am-tic-tac-toe-App-Native" className="title">XO: Tic Tac Toe</a>
-                            <div>[Work in Progress] Juego Tres en Línea creado con React Native para iOs y Android</div></div>
+                                <a href="https://github.com/florenciasilva/xoTicTacToe" className="title">XO: Tic Tac Toe</a>
+                                <div>Juego Tres en Línea creado con React Native para iOs y Android</div>
+                            </div>
                     </Card>
                     </Col>
 
                     <Col m={4} l={4}>
                     <Card className='small'
-                        header={<CardTitle image='https://i.imgur.com/Xd66I08.png'></CardTitle>}>
+                        header={<CardTitle image={Pokedeck}></CardTitle>}>
                             <div className="center">
-                            <a href="#" className="title">Calm.ly Social Network</a>
-                            <div>[Hosting in Progress] Red Social creada con React.</div></div>
+                            <a href="https://github.com/florenciasilva/talent-fest-grin" className="title">Pokedeck</a>
+                            <div>WebApp para creación de decks del juego de cartas PokemonTCG creada con React y Firebase.</div></div>
                     </Card>
                     </Col>
 
                     <Col m={4} l={4}>
                     <Card className='small'
-                        header={<CardTitle image='https://i.imgur.com/frwezZP.png'></CardTitle>}>
+                        header={<CardTitle image={Seventh}></CardTitle>}>
                             <div className="center">
-                            <a href="https://hackathon-here-f1d23.firebaseapp.com/" className="title">Sigue Bus</a>
-                            <div>WebApp Integral creada con React para administración de transporte escolar.</div></div>
+                            <a href="https://github.com/florenciasilva/seventh" className="title">Seventh</a>
+                            <div>[Work In Progress] Juego tipo Unfolding creado con React</div></div>
                     </Card>
                     </Col>
 
                     <Col m={4} l={4}>
                     <Card className='small'
-                        header={<CardTitle image='https://i.imgur.com/k29Zwnq.png'></CardTitle>}>
+                        header={<CardTitle image={Cipher}></CardTitle>}>
                             <div className="center">
-                            <a href="https://florenciasilva.github.io/lab-car-boilerplate/src/index.html" className="title">LabCar</a>
-                            <div>Maquetado Responsive para empresa de taxis ficticia.</div></div>
-                    </Card>
-                    </Col>
-                    <Col m={4} l={4}>
-                    <Card className='small'
-                        header={<CardTitle image='https://i.imgur.com/TDC9wR6.png'></CardTitle>}>
-                            <div className="center">
-                            <a href="https://florenciasilva.github.io/data-dashboard-laboratoria/src/" className="title">Data Dashboard</a>
-                            <div>Data Dashboard para Training Managers de Laboratoria</div></div>
+                                <a href="https://github.com/florenciasilva/cifrado-cesar" className="title">Caesars Cipher</a>
+                                <div>Cifrado cesar creado con JavaScript Vanilla</div>
+                            </div>
                     </Card>
                     </Col>
                     <Col m={4} l={4}>
