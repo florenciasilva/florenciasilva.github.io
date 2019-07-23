64f2027e5da88663e1f5c3267ad83b577422c00d
florenciasilva <hensgrej@gmail.com>
2019-07-23 15:14:22 -0500

    Updates

---
+++ precache-manifest.5a833f3907ab44a7fab6ffb35c3d7b77.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a15c7ebdcdc4e7d7e40a04d2f8f3a0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a11c4ca677544a9a8726",
+    "url": "/static/css/main.86a4b038.chunk.css"
+  },
+  {
+    "revision": "177a09694e7f1198d515",
+    "url": "/static/js/2.767fb0c1.chunk.js"
+  },
+  {
+    "revision": "a11c4ca677544a9a8726",
+    "url": "/static/js/main.cb5192db.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d3e1e38f92ea8ca13085c80ff26e7371",
+    "url": "/static/media/md-links.d3e1e38f.png"
+  },
+  {
+    "revision": "e455dfb643f5cae5198583cd517bf3e1",
+    "url": "/static/media/rainbow-flare.e455dfb6.png"
+  },
+  {
+    "revision": "b03355a4cb51485aa859e5ff22dc7ea7",
+    "url": "/static/media/rainbow.b03355a4.png"
+  }
+]);
